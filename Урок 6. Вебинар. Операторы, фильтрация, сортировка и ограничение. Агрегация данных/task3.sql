256f6547176311d95b3899c41d11ca847410a4b8
Virucek <aykin.yakov@gmail.com>
2020-03-05 21:53:58 +0300

    Add 2nd solution for lesson6_task3

--- Урок 6. Вебинар. Операторы, фильтрация, сортировка и ограничение. Агрегация данных/task3.sql
+++ Урок 6. Вебинар. Операторы, фильтрация, сортировка и ограничение. Агрегация данных/task3.sql
@@ -11,3 +11,24 @@ select
 		when 1 then 'male'
 		when 0 then 'female'
 	end as result;
+
+/* ИЛИ ВАРИНТ С ПРОВЕРКОЙ НА РАВЕНСТВО */
+
+select 
+	case (
+		(select count(*) from likes where user_id in (
+			select user_id from profiles where gender = 'M')) >
+		(select count(*) from likes where user_id in (
+			select user_id from profiles where gender = 'F'))
+		)
+		when 1 then 'male'
+		when 0 then case (
+			(select count(*) from likes where user_id in (
+				select user_id from profiles where gender = 'M')) =
+			(select count(*) from likes where user_id in (
+				select user_id from profiles where gender = 'F'))
+			)
+			when 1 then 'equals'
+			else 'female'
+			end
+	end as result;
